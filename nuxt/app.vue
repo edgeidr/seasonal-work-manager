48c5acb624e5a30d4bfdb9add6932be653f10451
edgeidr <edge.idr@gmail.com>
2024-10-30 23:51:03 +0800

    frontend init

---
+++ nuxt/app.vue
@@ -0,0 +1,9 @@
+<template>
+	<ScrollArea class="h-screen w-screen">
+		<div class="[ relative max-w-screen-lg mx-auto px-10 ][ desktop:min-h-screen ][ mobile:px-8 ]">
+			<NuxtLayout>
+				<NuxtPage />
+			</NuxtLayout>
+		</div>
+	</ScrollArea>
+</template>
